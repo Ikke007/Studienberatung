34fbabbe845bdb662c2a32d9b9404c316c9af24c
Hendrik <hendrikjanert@googlemail.com>
2015-04-16 10:49:35 +0200

    added Frage:Abi, input

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Studienberatung{
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+		String input = scanner.next();
 	}
 }
