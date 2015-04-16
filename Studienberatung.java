227428c228f5316420303bf82618a7657beb7023
Hendrik <hendrikjanert@googlemail.com>
2015-04-16 11:14:54 +0200

    added Frage: Hast du eine Berufsausbilung?

--- Studienberatung.java
+++ Studienberatung.java
@@ -9,6 +9,13 @@ public class Studienberatung{
 			System.out.println("Bist du wissbegierig?");
 		else if(input.equalsIgnoreCase("n")){
 			System.out.println("Hast du Fachabitur?");
-		}
+			input = scanner.next();
+			if (input.equalsIgnoreCase("j")){
+				System.out.println("Hast du eine Berufsausbildung?");
+			}
+			else{
+				
+			}
+		}	
 	}
 }
